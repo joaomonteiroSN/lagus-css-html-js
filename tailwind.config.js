8782d3e2b34b0fdfae86792f0c1ea56855d8b362
joao monteiro <70484799+joaomonteiroSN@users.noreply.github.com>
2023-06-07 18:01:04 -0300

    layout mobile incompleto

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'h1': ['Roboto', 'sans-serif']  
+    },
     extend: {
+      width: {
+        '283': '17.688rem',
+        '398': '24.875rem',
+        '400': '25rem'
+      },
+      height: {
+        '58': '3.625rem'
+      },
+      lineHeight: {
+        '24': '1.5rem',
+        '37': '2.344rem',
+      },
+      margin: {
+        '119': '7.459rem',
+        '82': '5.125rem'
+      },
+      colors: {
+        'header-color': '#070707',
+        'legend-color': '#000000',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
