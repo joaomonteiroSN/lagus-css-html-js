47833c1a23f9dfbd37f730bb4ab349db5dbe4f4a
joao monteiro <70484799+joaomonteiroSN@users.noreply.github.com>
2023-06-08 12:44:20 -0300

    layout desktop incompleto

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,17 @@ export default function Home() {
     <body>
       <div class="">
         {/* travar posição no mq desktop */}
-        <header class="h-58 sticky top-0 flex grid-cols-1 bg-header-color justify-between items-center"  >
-            <div class="ml-4">
+        <header class="h-58 sticky top-0 flex grid-cols-1 bg-header-color justify-between items-center z-10"  >
+          <div class="ml-4">
             <Image
               src="menu.svg"
               width={18}
               height={18}
               alt="icon of menu"
             />
-            </div>
+          </div>
           <div class="ml-119 mr-82 ">
-          <Image
+            <Image
               src="lagus-logo.svg"
               width={118}
               height={48}
@@ -42,20 +42,24 @@ export default function Home() {
 
         </header>
 
-        <h1 class="text-3xl font-h1 font-bold w-283 m-auto mb-16 mt-4 leading-37">This page is a HTML/CSS/JS Test!</h1>
+        <h1 class="text-3xl font-h1 font-bold text-32 text-center w-283 h-117 m-auto mb-16 mt-16 leading-37 desktop:w-743 desktop:h-14 desktop:leading-56 desktop:text-46 desktop:mt-24 desktop:mb-24 ">This page is a HTML/CSS/JS Test!</h1>
 
-        <p class="w-398 font-h1 font-normal text-base text-justify m-auto mb-6">The objective of this test is to assess your coding and styling skills. It will be considered a good result if you can style all the components of this page. This text also count as a valid component.</p>
+        <div class="desktop:flex desktop:grid-cols-2, gap-x-8 desktop:w-606 desktop:m-auto desktop:mb-24">
 
-        <p class="w-398 font-h1 font-normal text-base text-justify m-auto mb-16">This second part is a second paragraph of the same component. Both p tags must follow the flow and display side by side, instead of the regular behavior of one on top of the other. only on desktop.</p>
+          <p class="w-398 font-h1 font-normal text-base text-justify m-auto mb-6 desktop:m-0">The objective of this test is to assess your coding and styling skills. It will be considered a good result if you can style all the components of this page. This text also count as a valid component.</p>
 
-        <span class="block w-398 font-h1 font-thin text-2xl leading-7 text-center m-auto mb-16">Attention: components must be responsive and use CSS Flex and Grid layouts</span>
+          <p class="w-398 font-h1 font-normal text-base text-justify m-auto mb-16 desktop:m-0">This second part is a second paragraph of the same component. Both p tags must follow the flow and display side by side, instead of the regular behavior of one on top of the other. only on desktop.</p>
+        </div>
 
-        <h1 class="text-3xl text-center font-h1 font-bold w-283 m-auto mb-8 leading-37">Image List Component</h1>
 
-        <div class="w-400 m-auto flex flex-col grid-cols-1 gap-y-4">
+        <span class="block w-398 font-thin text-2xl leading-7 tracking-wider text-center m-auto mb-16">Attention: components must be responsive and use CSS Flex and Grid layouts</span>
 
-          <figure class="img-wrapper">
-          <span class="inline-flex items-center w-400 pl-4 h-11 absolute bg-legend-color opacity-75 font-h1 text-base align-middle">Atom in the hand of a robot</span>
+        <h1 class="text-3xl text-center font-h1 font-bold w-283 m-auto mb-8 leading-37 desktop:w-489 desktop:text-5xl desktop:mb-12">Image List Component</h1>
+
+        <div class="w-400 m-auto flex flex-col grid-cols-1 gap-y-4 desktop:w-1024 desktop:grid desktop:grid-cols-2 desktop:grid-rows-2 desktop:gap-6">
+
+          <figure class="w-400">
+            <span class="inline-flex items-center w-400 pl-4 h-11 absolute bg-legend-color opacity-75 font-h1 text-base align-middle">Atom in the hand of a robot</span>
             <Image
               src="atom-pic.svg"
               width={400}
@@ -63,8 +67,8 @@ export default function Home() {
               alt="robot hand and a atom" />
           </figure>
 
-          <figure class="img-wrapper">
-          <span class="inline-flex items-center w-400 pl-4 h-11 absolute bg-legend-color opacity-75 font-h1 text-base align-middle">Person working on a website</span>
+          <figure class="w-606">
+            <span class="inline-flex items-center w-400 pl-4 h-11 absolute bg-legend-color opacity-75 font-h1 text-base align-middle">Person working on a website</span>
             <Image
               src="pic-man-code.svg"
               width={400}
@@ -72,13 +76,13 @@ export default function Home() {
               alt="man and a laptop" />
           </figure>
 
-          <figure class="img-wrapper rounded-lg">
+          <figure class="w-400 rounded-lg">
             <span class="inline-flex items-center w-400 pl-4 h-11 absolute bg-legend-color opacity-75 font-h1 text-base align-middle">Women working in styles and ESLint configurations</span>
             <Image
               src="code-2.svg"
               width={400}
               height={400}
-              alt="woman with headphones coding"/>
+              alt="woman with headphones coding" />
           </figure>
 
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,18 +13,29 @@ module.exports = {
       width: {
         '283': '17.688rem',
         '398': '24.875rem',
-        '400': '25rem'
+        '400': '25rem',
+        '489': '32.5rem',
+        '606': '37.875rem',
+        '743': '46.438rem',
+        '1024': '64rem',
+        'desk': '90rem',
       },
       height: {
-        '58': '3.625rem'
+        '58': '3.625rem',
+        '117': '7.313rem'
       },
       lineHeight: {
         '24': '1.5rem',
         '37': '2.344rem',
+        '56': '3.516rem',
       },
       margin: {
         '119': '7.459rem',
         '82': '5.125rem'
+      },
+      fontSize: {
+        '32': '2rem',
+        '46': '2.875rem'
       },
       colors: {
         'header-color': '#070707',
@@ -35,6 +46,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    screens: {
+      'desktop': '1440px',
     },
   },
   plugins: [],
